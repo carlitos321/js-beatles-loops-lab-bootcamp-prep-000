24cf51e0b8250433aae8c0d2e75df28baa22dd5b
carlitos321 <ca.cabrera321@gmail.com>
2018-02-01 22:22:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
 
 function theBeatlesPlay(musicians, instruments) {
   const musicianDescribers = [];
@@ -8,4 +14,22 @@ function theBeatlesPlay(musicians, instruments) {
     musicianDescribers.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return musicianDescribers;
+}
+
+function johnLennonFacts(facts) {
+  while() {
+
+  }
+
+}
+
+function iLoveTheBeatles(num) {
+ const repeatedSentence = [];
+  do {
+
+  }
+  while() {
+
+  }
+
 }
